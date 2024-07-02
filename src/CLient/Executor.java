52367e81c6020247f87d1b828e4005a1fabb53c7
Rockmate77 <52208570+Rockmate77@users.noreply.github.com>
2024-07-02 09:16:47 +0200

    Mise en place du main et de l'archi de fichiers

---
+++ src/CLient/Executor.java
@@ -0,0 +1,16 @@
+package CLient;
+
+import CLient.Controler.Client;
+/**
+ * 
+ */
+public class Executor {
+    /**
+     * Main
+     */
+    public static Client cli;
+    public static void main() {
+        cli=new Client();
+    }
+
+}
